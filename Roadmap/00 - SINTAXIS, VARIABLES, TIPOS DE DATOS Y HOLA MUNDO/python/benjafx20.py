f8068e5595b68841ec486f582f03195181563612
benjafx20 <benjamincatalan.trd@gmail.com>
2024-12-17 19:07:13 -0300

    Create benjafx20.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/benjafx20.py
@@ -0,0 +1,35 @@
+# https://python.org
+
+#comentario en solo una linea
+
+"""
+Y esto se supone
+es un comentario
+en varias lineas
+"""
+
+''' 
+y este ess otra forma
+solo con comilla
+ simple
+'''
+
+my_variable = "mi variable perrona"
+
+'''
+no tiene constante en python 
+pero si creo una constante por convencion 
+pero se podria cambiar jaja
+'''
+my_constant = " serviria solo para alertar que no se debe cambiar"
+
+# datos primitivos
+my_int = 1 #enteros
+my_floot = 1.5 #decimal
+my_bool = true
+my_bool = false # boolianos verdadero o falso
+my_string = "cadena de texto" #cadenita de texto
+my_string = 'otra forma con comilla simple'
+# fin de lenguaje primitivo
+
+print("Hola, python es mi primera vez con este lenguaje ")
